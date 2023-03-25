12e3532b7f82f71f8374be71735db4c699640350
ismail tahiri <ismailtahiri2002@gmail.com>
2023-03-25 16:28:26 +0100

    create project and migrations

---
+++ database/migrations/2023_03_23_172826_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id('idComment');
+           
+            $table->unsignedBigInteger('idUser');
+            $table->foreign('idUser')->references('idUser')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('idTweet');
+            $table->foreign('idTweet')->references('idTweet')->on('tweets')->onDelete('cascade');
+            $table->longText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
